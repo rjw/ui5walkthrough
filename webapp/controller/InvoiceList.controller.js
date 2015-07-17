a9244fa5a4efadc7c57c0387ae634b5282c27db6
rjwilmot <r.wilmot.72@gmail.com>
2015-07-17 15:22:36 +0100

    Up to Step 25

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,8 +1,10 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
   "sap/ui/model/json/JSONModel",
-  "sap/ui/demo/wt/model/formatter"
-], function(Controller, JSONModel, formatter) {
+  "sap/ui/demo/wt/model/formatter",
+  "sap/ui/model/Filter",
+  "sap/ui/model/FilterOperator"
+], function(Controller, JSONModel, formatter, Filter, FilterOperator) {
   "use strict";
 
   return Controller.extend("sap.ui.demo.wt.controller.InvoiceList", {
@@ -11,6 +13,21 @@ sap.ui.define([
     onInit : function() {
       var oViewModel = new JSONModel({currency: "GBP"});
       this.getView().setModel(oViewModel, "view");
+    },
+
+    onFilterInvoice : function(oEvent) {
+      var aFilter = [];
+      var sQuery = oEvent.getParameter("query");
+
+      // Build a list of all matching ProductNames
+      if (sQuery) {
+        aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+      }
+
+      // Filter binding
+      var oList = this.getView().byId("invoiceList");
+      var oBinding = oList.getBinding("items");
+      oBinding.filter(aFilter);
     }
   });
 });
